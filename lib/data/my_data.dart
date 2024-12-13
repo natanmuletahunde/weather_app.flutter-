0b59b39490a6f9a59765a49040e17e1ed0461932
pivnati <natanmuleta911@gmail.com>
2024-12-13 15:22:21 -0700

    create the data

---
+++ lib/data/my_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyData extends StatelessWidget {
+  const MyData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
