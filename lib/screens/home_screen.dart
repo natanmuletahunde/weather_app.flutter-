2c96e60293278c277fe6f5f38654edd7f70dd0b9
pivnati <natanmuleta911@gmail.com>
2024-12-13 16:07:29 -0700

    create the app bar in the home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:Colors.black,
+      extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor:Colors.black,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+           statusBarBrightness: Brightness.dark, 
+          ),
 
-      
-    );
+
+        ),
+    );  
   }
 }
