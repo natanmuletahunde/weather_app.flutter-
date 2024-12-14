b8707ef681e662ab6736971d0bf87462d149995d
pivnati <natanmuleta911@gmail.com>
2024-12-13 21:07:16 -0700

    make the date , condition of the weather , the temperature

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,103 +2,125 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Colors.black,
+      backgroundColor: Colors.black,
       extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          backgroundColor:Colors.transparent,
-          elevation: 0,
-          systemOverlayStyle: const  SystemUiOverlayStyle(
-           statusBarBrightness: Brightness.dark, 
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.dark,
         ),
-        body: Padding(padding: EdgeInsets.fromLTRB(
-          40,1.2*kToolbarHeight,40,20
-        ),
-        child:SizedBox(
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: SizedBox(
           height: MediaQuery.of(context).size.height,
-          child:Stack(
+          child: Stack(
             children: [
               Align(
                 alignment: AlignmentDirectional(3, -0.3),
-                child:Container(
-                height:250,
-                width: 250,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.deepPurple,
-
-                ),  
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepPurple,
+                  ),
                 ),
               ),
-               Align(
+              Align(
                 alignment: AlignmentDirectional(-3, -0.3),
-                child:Container(
-                height:250,
-                width: 250,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.deepPurple,
-                ),  
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepPurple,
+                  ),
                 ),
               ),
-               Align(
+              Align(
                 alignment: AlignmentDirectional(0, -1.2),
-                child:Container(
-                height:250,
-                width: 600,
-                decoration: BoxDecoration(
-                 
-                  color: Color(0xFFFFAB40),
-                  
-                ),  
+                child: Container(
+                  height: 250,
+                  width: 600,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFAB40),
+                  ),
                 ),
               ),
-              BackdropFilter(filter: 
-              ImageFilter.blur(
-                sigmaX: 100.0,sigmaY: 100.0
-              ),
-              child: Container(
-                
-                decoration: const BoxDecoration(
-                  color: Colors.transparent
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
+                child: Container(
+                  decoration: const BoxDecoration(color: Colors.transparent),
                 ),
-              ),
               ),
               SizedBox(
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
-                child:  Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,// make the column to start from the  the begin of the page 
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment
+                      .start, // make the column to start from the  the begin of the page
                   children: [
-                   const  Text(' hi piv nati',
-                    style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 1),
-                      fontWeight: FontWeight.w300
+                    const Text(
+                      ' hi piv nati',
+                      style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                          fontWeight: FontWeight.w300),
                     ),
+                    const SizedBox(
+                      height: 4,
                     ),
-                 const   SizedBox(height: 4,),
-                   const  Text('Good Morning',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold
+                    const Text(
+                      'Good Morning',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
                     ),
+                    Image.asset('assets/1.png'),
+                    const Center(
+                      child: Text(
+                        '21°C',
+                        style: TextStyle(
+                            color:Colors.white,
+                            fontSize: 55,
+                            fontWeight: FontWeight.w600),
+                      ),
                     ),
-                    Image.asset('assets/1.png')
+                     const Center(
+                      child: Text(
+                        'ThunderStorm',
+                        style: TextStyle(
+                            color:Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                     const Center(
+                      child: Text(
+                        'Friday 16. 09.41am',
+                        style: TextStyle(
+                            color:Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                    const SizedBox(height: 30,)
                   ],
                 ),
               )
             ],
           ),
-        ) ,
-        
         ),
-    );  
+      ),
+    );
   }
 }
